3db39efcb87b4e810047e1f647860cc40570fd2f
Benzenec6h6 <aconitinec34h47no11@gmail.com>
2026-04-03 03:31:08 +0900

    Keep ISO up to date

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "Custom NixOS Live ISO with flakes & sandbox disabled";
+  description = "Custom NixOS Live ISO with flakes & latest kernel";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,55 +16,43 @@
   outputs = { self, nixpkgs, disko, sops-nix, ... }: {
     nixosConfigurations.custom-iso = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-
       modules = [
         "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-base.nix"
         disko.nixosModules.disko
         sops-nix.nixosModules.sops
 
         ({ pkgs, ... }: {
-
           # --- Nix 設定 ---
           nix.settings = {
-            extra-experimental-features = [
-              "nix-command"
-              "flakes"
-            ];
+            extra-experimental-features = [ "nix-command" "flakes" ];
             download-buffer-size = 536870912;
             max-substitution-jobs = 8;
           };
 
-          # --- DNS ---
+          # --- ハードウェア互換性 / カーネル ---
+          boot.kernelPackages = pkgs.linuxPackages_latest;
+          boot.supportedFilesystems = [ "zfs" "btrfs" "ext4" "vfat" "ntfs" "exfat" ];
+
+          # --- ネットワーク設定 ---
+          networking.networkmanager.enable = true;
+          networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
           services.resolved.enable = true;
 
-          environment.etc."resolv.conf".source =
-            "/run/systemd/resolve/stub-resolv.conf";
+          # --- 便利サービス (SSH) ---
+          services.openssh.enable = true;
 
-          networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
-
-          # NetworkManager を使う（DHCP を含む）
-          networking.networkmanager.enable = true;
-
-          # --- live 環境のツール ---
+          # --- システムパッケージ ---
           environment.systemPackages = with pkgs; [
-            git
-            curl
-            wget
-            pciutils
-            usbutils
-            sops
-            age
-            ssh-to-age
-            whois
-            gnused
-            gawk
-            sudo 
-            vim
-            nano
+            git curl wget pciutils usbutils
+            sops age ssh-to-age
+            whois gnused gawk
+            vim nano
             disko.packages.${pkgs.stdenv.hostPlatform.system}.disko
           ];
 
-          image.fileName = "nixos-custom-flakes.iso";
+          # ISOファイル名の指定
+          isoImage.isoName = "nixos-custom-flakes.iso";
+          # image.fileName は古い形式のことがあるため、isoImage.isoName を推奨
         })
       ];
     };
